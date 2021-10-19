3ba612694cdfccec8e39306eb198e1063b68b38c
omoluabidotcom <yyahaya222@yahoo.com>
2021-10-19 10:56:42 -0700

    resthandler was adjusted to return response body when it methods are been called

--- config/Resthandler.php
+++ config/Resthandler.php
@@ -6,15 +6,20 @@ class Resthandler{
 
         $response['status_header_code'] = 'HTTP/1.1 404 Not Found';
         $response['body'] = null;
+
+        print_r($response['body']);
     }
 
     public function unprocessable() {
 
-        $response['status_header_code'] = 'HTTP/1.1 Unprocessable Entity';
+        $response['status_header_code'] = 'HTTP/1.1 422 Unprocessable Entity';
         $response['body'] = json_encode(array(
             "error" => "Invalid Input"
         ));
+
+        print_r($response['body']);
     }
+
 }
 
 ?>
